56fb8a489261e58180434bf8eeb805ecb009c9d2
sourab <sourabhjadav7@gmail.com>
2022-06-23 00:08:00 +0530

    add formatters for cpp and html

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,19 +1,40 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+
+local sources = { null_ls.builtins.diagnostics.cppcheck }
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+  debug = false,
+  sources = {
+    --formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    --formatting.black.with({ extra_args = { "--fast" } }),
+    --formatting.stylua,
     -- diagnostics.flake8
-	},
+    formatting.prettier, sources.cppcheck, formatting.black, formatting.tidy, formatting.gofmt, formatting.shfmt,
+    formatting.clang_format, formatting.cmake_format, formatting.dart_format,
+    formatting.lua_format.with({
+      extra_args = {
+        '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
+        '--break-after-table-lb', '--indent-width=2'
+      }
+    }), formatting.isort, formatting.codespell.with({ filetypes = { 'markdown', 'html', 'css' } })
+  },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
+    end
+    vim.cmd [[
+      augroup document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]]
+  end
 })
